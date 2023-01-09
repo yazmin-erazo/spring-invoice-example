1430a29f6425c2ee9ae9d900148e4f7954a729e8
Yazmin Erazo <54871751+yazmin-erazo@users.noreply.github.com>
2023-01-09 11:28:49 -0500

    Improving comments

--- src/main/java/com/ideas/springboot/app/controller/PathVariablesController.java
+++ src/main/java/com/ideas/springboot/app/controller/PathVariablesController.java
@@ -12,33 +12,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/variables")
 public class PathVariablesController {
 
-    /*El método, 'index', maneja solicitudes GET a la ruta '/variables/' y agrega un atributo 'title' al modelo. Luego, devuelve la vista 'variables/index'.*/
+    //Maneja solicitudes GET a la ruta '/variables/' y agrega un atributo 'title' al modelo.
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("title", "Enviar parámetros de la ruta (@PathVariable)");
+        //Devuelve la vista 'variables/index'.
         return "variables/index";
     }
 
-    /*El método, 'variables', maneja solicitudes GET a la ruta '/variables/string/{text}' y agrega dos atributos al modelo: 'title' y 'result'.
-    'text' es un parámetro de ruta y se recibe a través del parámetro de anotación @PathVariable. 'result' es un mensaje que incluye el valor de 'text'.
-    Luego, devuelve la vista 'variables/ver'*/
+    /*Maneja solicitudes GET a la ruta '/variables/string/{text}' y agrega dos atributos al modelo: 'title' y 'result'.
+    'text' es un parámetro de ruta y se recibe a través del parámetro de anotación @PathVariable.*/
     @GetMapping("/string/{text}")
     public String variables(@PathVariable String text, Model model){
         model.addAttribute("title", "Recibir parámetros del Request (@PathVariable)");
+        //Mensaje que incluye el valor de 'text'.
         model.addAttribute("result", "El texto enviado en la ruta es: " + text);
+        //Devuelve la vista 'variables/ver'
         return "variables/ver";
     }
 
-    /*El metodo 'variables', maneja solicitudes GET a la ruta '/variables/string/{text}/{number}' y es similar al segundo método,
-    pero también recibe un parámetro de ruta adicional llamado 'number' a través del parámetro de anotación @PathVariable.
-    El mensaje en el atributo 'result' incluye tanto el valor de 'text' como el de 'number'. Luego, devuelve la vista 'variables/ver'*/
+    /*Maneja solicitudes GET a la ruta '/variables/string/{text}/{number}' y es similar al segundo método,
+    pero también recibe un parámetro de ruta adicional llamado 'number' a través del parámetro de anotación @PathVariable.*/
     @GetMapping("/string/{text}/{number}")
     public String variables(@PathVariable String text, @PathVariable Integer number, Model model){
         model.addAttribute("title", "Recibir parámetros del Request (@PathVariable)");
+        //mensaje en el atributo 'result' incluye tanto el valor de 'text' como el de 'number'
         model.addAttribute("result",
                 "El texto enviado en la ruta es: " + text
                         + " y el numer enviado en el path es: " + number);
+        //Devuelve la vista 'variables/ver'
         return "variables/ver";
     }
-
 }
